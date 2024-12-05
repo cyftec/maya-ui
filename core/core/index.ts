706e934655ae41ba4c022edaf1c60d00df4cd2ce
chnkrydv <chnkrydv@gmail.com>
2024-12-05 13:26:25 +0530

    fix: global import throwing error, remove import

--- core/core/index.ts
+++ core/core/index.ts
@@ -1,6 +1,5 @@
 export * from "./build/index";
 export * from "./building-blocks/index";
 export * from "./dom/index";
-export * from "./global";
 export * from "./types";
 export * from "./utils/index";
