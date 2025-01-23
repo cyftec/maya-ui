889dda3494664455e1ad92806704be2576fb580b
chnkrydv <chnkrydv@gmail.com>
2025-01-23 19:40:55 +0530

    build: update latest maya version in code

--- brahma/src/utils/constants.ts
+++ brahma/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const NPM_DEPS = {
-  MAYA: { "@mufw/maya": "0.1.8" },
+  MAYA: { "@mufw/maya": "0.1.9" },
   CHROME: { "@types/chrome": "0.0.297" },
   PWA: { "@types/web-app-manifest": "1.0.8" },
 };
