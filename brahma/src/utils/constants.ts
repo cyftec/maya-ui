4477803e7ca79b2c61cabd88c9ad84797291e81a
chnkrydv <chnkrydv@gmail.com>
2025-01-24 18:51:02 +0530

    build: publish maya v0.1.10

--- brahma/src/utils/constants.ts
+++ brahma/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const NPM_DEPS = {
-  MAYA: { "@mufw/maya": "0.1.9" },
+  MAYA: { "@mufw/maya": "0.1.10" },
   CHROME: { "@types/chrome": "0.0.297" },
   PWA: { "@types/web-app-manifest": "1.0.8" },
 };
