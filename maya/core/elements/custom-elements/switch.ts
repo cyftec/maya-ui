e8a384de39d396f8d6b2355319df7a0a8972c6ee
chnkrydv <chnkrydv@gmail.com>
2025-02-02 04:21:20 +0530

    feat: implement case matcher for cases where case-key doesn't directly match with subject but follows a pattern

--- maya/core/elements/custom-elements/switch.ts
+++ maya/core/elements/custom-elements/switch.ts
@@ -1,30 +1,45 @@
 import {
   derived,
   val,
+  valueIsSignal,
   type DerivedSignal,
-  type MaybeSignalValue,
+  type Signal,
 } from "@cyftech/signal";
 import type { Child } from "../../../index.types.ts";
 import { m } from "../m.ts";
 
-type SwitchProps = {
-  subject: MaybeSignalValue<string>;
+type SwitchReturn<Subject> = Subject extends Signal<any>
+  ? DerivedSignal<Child>
+  : Child;
+
+export type SwitchElement = <S>(props: {
+  subject: S;
+  caseMatcher?: (subject: S, matchingCase: string) => boolean;
   defaultCase?: Child;
   cases: {
     [x in string]: Child;
   };
-};
-export type SwitchElement = (props: SwitchProps) => DerivedSignal<Child>;
+}) => SwitchReturn<S>;
 
 export const switchElement: SwitchElement = ({
   subject,
+  caseMatcher,
   defaultCase,
   cases,
-}): DerivedSignal<Child> => {
-  const switchCase = derived(() => val(subject));
+}): SwitchReturn<typeof subject> => {
+  const switchReturnGetter = () => {
+    const subjectValue = val(subject);
+    let component: Child | undefined = undefined;
+    for (const [key, comp] of Object.entries(cases)) {
+      if ((caseMatcher && caseMatcher(subject, key)) || subjectValue === key) {
+        component = comp;
+        break;
+      }
+    }
+    return component || defaultCase || m.Span({ style: "display: none;" });
+  };
 
-  return derived(() => {
-    const caseKey = switchCase.value;
-    return cases[caseKey] || defaultCase || m.Span({ style: "display: none;" });
-  });
+  return (
+    valueIsSignal(subject) ? derived(switchReturnGetter) : switchReturnGetter()
+  ) as SwitchReturn<typeof subject>;
 };
