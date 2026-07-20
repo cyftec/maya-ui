e9cb1991949fff36deac00e8b602e8be3b7b7782
chnkrydv <chnkrydv@gmail.com>
2026-07-20 12:54:16 +0530

    chore: remove comments from tutorial pages

--- website/dev/tutorial/@libs/constants.ts
+++ website/dev/tutorial/@libs/constants.ts
@@ -56,42 +56,22 @@ export const TUTORIAL_CHAPTERS = [
         article: InstallationAndSetup,
       },
       {
-        // show how to bootstrap an app using brahma
-        // also tell different modes in which app
-        // can be bootstrapped like 'web', 'pwa' and 'ext'
-        // then as an example create a sample web app
         title: "Your first app using 'brahma create'",
         article: FirstApp,
       },
       {
-        // show the app structure alonside karma.ts files
-        // and the app directory structure of the sample
-        // app bootstrapped by 'brahma create'
-        // detailed karma.ts structure is discussed in
-        // subsequent chapters
-        // folder-based routes
-        // assets folder gets generated exactly during build
         title: "App structure",
         article: AppStructure,
       },
       {
-        // basic overview of Maya as a rendering engine
-        // rest of the specifics are covered below in
-        // subsequent chapters ('Sample App' section)
         title: "Maya",
         article: Maya,
       },
       {
-        // how all the configs stored here in karma.ts
-        // and broadly what all config values mean
-        // also show that how it's in TS and not in JSON
-        // so that same variable can be reused
         title: "Karma",
         article: Karma,
       },
       {
-        // how brahma takes help of karma to spawn
-        // and stages the app for continuous development
         title: "Brahma",
         article: Brahma,
       },
@@ -138,18 +118,10 @@ export const TUTORIAL_CHAPTERS = [
     title: "Reactivity",
     topics: [
       {
-        // Maya can totally be used to generate
-        // static html pages
-        // but also built with @cyftec/signal under
-        // the hood. SO reactivity in Maya
-        // apps can be acheived by using signals
         title: "Static vs Reactive Maya app",
         article: StaticVsReactive,
       },
       {
-        // Introduction only
-        // custom implementation specifically for
-        // Maya. Comprehensive intuitive api.
         title: "What is signal?",
         article: WhatIsSignal,
       },
