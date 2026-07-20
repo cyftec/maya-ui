9704fc07a2b6ea45da3d71b5f4543fb1e37db246
chnkrydv <chnkrydv@gmail.com>
2026-07-20 19:50:06 +0530

    docs: document the list of capablities need to be implemented for signalled 'query' api

--- maya/toolkit/query.ts
+++ maya/toolkit/query.ts
@@ -1,5 +1,36 @@
 import { effect, promstates, signal } from "@cyftec/signal";
 
+/**
+ * TODO: Implement below modern querying features
+ *
+ * 1. Caching of fetched data
+ *    - for instant data load
+ *
+ * 2. Retry logic
+ *    - for an uninterrupted and more promising fetch
+ *
+ * 3. Request Deduplication
+ *    - if multiple events of component loads triggers the same fetch
+ *
+ * 4. Automatic Background Refetching
+ *    - when the user switches tabs back to your app, or
+ *    - when they regain their internet connection
+ *
+ * 5. Optimistic or anticipatory fetching (Infinite Scrolling / Pagination State)
+ *    - when an event confirms that the next fetch is required
+ *
+ * 6. Dependent Queries
+ *    - where one API call requires a piece of data from a previous API call
+ *
+ * 7. Auto-Cancellation (Race Condition Prevention)
+ *    - If a component unmounts mid-request, or
+ *    - if a user triggers a new search before the first search finishes
+ *
+ * 8. Return-Stale-While-Revalidate
+ *    - return data instantly from the cache so the user has something to look at,
+ *    - silently fetch the fresh data and update the UI
+ */
+
 type QueryState<D> = {
   isLoading: boolean;
   data: D | undefined;
